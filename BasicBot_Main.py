79033b9f2fce7d783a7709221dfd112a4ff024da
Brad Estus <50963179+UnderscoreBrad@users.noreply.github.com>
2021-01-13 21:05:20 -0500

    Join/Leave fully works!

--- BasicBot_Main.py
+++ BasicBot_Main.py
@@ -7,8 +7,10 @@ import random
 import string
 import sys
 
-bot = Bot(command_prefix='!')
-OWNER = 210455720137588737
+intents = discord.Intents.default()
+intents.members = True
+bot = Bot(command_prefix='!', intents=intents)
+OWNER_ID = [YOUR ID]
 TOKEN = None
 try:
     fp = open('config.txt')
@@ -20,21 +22,18 @@ except:
 finally:
     fp.close()
     print('Starting BasicBot.')
-
-#client = discord.Client()
+    
 terminateCode = '!basicbot_terminate '+''.join(random.choices(string.ascii_uppercase + string.digits, k=16))
-global currentVoice_Client 
-currentVoice_Client = None
 
 @bot.event
 async def on_ready():
+    global OWNER_ID
     for guild in bot.guilds:
         print(f'BasicBot_PY joined server: {guild.name} ID: {guild.id}')
-    OWNER = bot.get_user(210455720137588737)
     print(f'{bot.user} is online. Terminate with OTP: {terminateCode}')
-    print(f'{OWNER} detected as Bot Owner. Change in BasicBot_Main.py')
-    #await OWNER.create_dm()
-    #await OWNER.dm_channel.send(f'{bot.user} is online. Terminate with OTP: {terminateCode}')
+    print(f'{bot.get_user(OWNER_ID)} detected as Bot Owner. Change in BasicBot_Main.py')
+    await bot.get_user(OWNER_ID).create_dm()
+    await bot.get_user(OWNER_ID).dm_channel.send(f'{bot.user} is online. Terminate with OTP: {terminateCode}')
 
 @bot.event
 async def on_message(message):
@@ -54,31 +53,27 @@ async def on_message(message):
             response = f'Joining voice channel {message.author.voice.channel}'
             print(f'{message.author} asked {bot.user} to join {message.author.voice.channel}')
             await channel.connect()
-            currentVoice_Client = channel
             await message.channel.send(response)
         elif message.content == '!basic_leaveVC':
             response = f'Leaving voice channel {message.author.voice.channel}'
             print(f'{message.author} asked {bot.user} to leave {message.author.voice.channel}')
             vcID = message.guild.voice_client
             await vcID.disconnect()
-            currentVoice_Client = None
             await message.channel.send(response)
         elif message.content == terminateCode:
             await message.channel.send('BasicBot_PY is shutting down!')
-            await currentVoice_Client.disconnect()
             exit();
 
 @bot.event
 async def on_voice_state_update(member, before, after):
-    #bot.get_user(798647715473915924)
-    if after.channel == currentVoice_Client:
+    voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients)
+    channel = voice_client.channel
+    if before.channel == channel and after.channel != channel:
         print("USER LEFT")
-        voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients)
         player = discord.FFmpegPCMAudio('AudioBin/LeaveSound.mp3')
         voice_client.play(player, after=None)
-    elif after.channel != currentVoice_Client:
+    elif before.channel != channel and after.channel == channel:
         print("USER JOINED")
-        voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients)
         player = discord.FFmpegPCMAudio('AudioBin/JoinSound.mp3')
         voice_client.play(player, after=None)
    
@@ -87,7 +82,6 @@ try:
 except:
     print("Error running your bot. Check BOT_TOKEN in config.txt")
 finally:
-    
     print("Thank you for using BasicBot_PY.")
 
 
